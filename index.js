b103b7658148459ef03a4ad2868850bdd4e3cdf6
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 23:17:56 +0530

    Add middleware to use res.body

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT;
 
+// To use req.body
+app.use(express.json());
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
