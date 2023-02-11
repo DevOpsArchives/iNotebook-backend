88620168a164cc4f33918f859689c4c61e2732f3
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 20:12:28 +0530

    Add database schema for Notes and Users collection

---
+++ models/Notes.js
@@ -0,0 +1,23 @@
+const mongo = require("mongoose");
+
+const NotesSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tag: {
+    type: String,
+    default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongo.model("user", NotesSchema);
