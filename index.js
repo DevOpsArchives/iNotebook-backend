be97caffbaff964758161d0ed596358666c03382
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-02-11 20:13:01 +0530

    Organize project structure

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 // Running App
 app.listen(port, () => {
